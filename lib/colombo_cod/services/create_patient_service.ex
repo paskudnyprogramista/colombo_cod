820f6509509afec7cdc0b94af3e891663d236feb
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-06-10 00:27:03 +0200

    Generate CreatePatientService

---
+++ lib/colombo_cod/services/create_patient_service.ex
@@ -0,0 +1,17 @@
+defmodule ColomboCodModule.Services.CreatePatientService do
+  @moduledoc false
+
+  def call(patient_changeset) do
+    # TODO: Impl
+    # Fetch patient
+    # If patient exists, return failure
+    # Else
+    # Fetch patient_invitation_notification where patient.phone == patient_invitation_notification.phone
+    # If patient_invitation_notification exists
+    # Update patient_invitation_notification with patient
+    # Else
+    # Do nothing
+
+    Multi.new
+  end
+end
