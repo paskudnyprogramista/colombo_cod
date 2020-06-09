aea7fc0c23e57940e225f7bf65a462d3466132f0
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-06-09 23:50:51 +0200

    Add patient association builder
    Purpose of this function is to build and insert patient_invitation_notification with patient phone number.

--- lib/colombo_cod/services/notify_patient_service.ex
+++ lib/colombo_cod/services/notify_patient_service.ex
@@ -2,10 +2,17 @@ defmodule ColomboCodModule.Services.NotifyPatientService do
   @moduledoc false
 
   def call(patient) do
+    Task.start(fn -> persist_patient_invitation_notification(patient) end)
     Task.start(fn -> notify(patient) end)
   end
 
   defp notify(patient) do
     IO.puts("Send SMS to: #{patient.phone}")
+  end
+
+  defp persist_patient_invitation_notification(patient) do
+    Ecto.build_assoc(patient, :patient_invitation_notification)
+    |> Ecto.Changeset.change(phone: patient.phone)
+    |> ColomboCodModule.Repo.insert!()
   end
 end
