fc865aa60e4a95c89562465c6295b74d0e143990
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-06-12 01:46:43 +0200

    Add test for checking if background job is run in intervals

--- test/colombo_cod/workers/notify_patients_worker_test.exs
+++ test/colombo_cod/workers/notify_patients_worker_test.exs
@@ -6,10 +6,16 @@ defmodule ColomboCodModule.Workers.NotifyPatientsWorkerTest do
 
   describe "handle_info/2" do
     test "receive :work message" do
-      assert :ko == :ok
+      worker_pid = Process.whereis(ColomboCodModule.Workers.NotifyPatientsWorker)
+
+      :erlang.trace(worker_pid, true, [:receive])
+
+      assert_receive {:trace, ^worker_pid, :receive, :work}, 3_600
     end
 
     test "returns {:noreply, :ok} tuple" do
+      # TODO: Check if it's possible to get 
+      
       assert :ko == :ok
     end
   end
