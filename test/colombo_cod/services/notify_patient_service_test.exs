7b0158617bd7d346880e97406fc9fd24df6c272f
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-06-11 23:40:14 +0200

    Add base patient service test

--- test/colombo_cod/services/notify_patient_service_test.exs
+++ test/colombo_cod/services/notify_patient_service_test.exs
@@ -1,26 +1,47 @@
-defmodule ColomboCodModule.Workers.NotifyPatientsWorkerTest do
+defmodule ColomboCodModule.Workers.NotifyPatientsServiceTest do
   @moduledoc false
 
   use ExUnit.Case, async: true
   use ColomboCodModule.DataCase, async: true
 
+  import ColomboCodModule.Factory
+
   describe "call/1" do
-    test "spawns new processes" do
+    test "spawns notify process" do
+      # TODO: Impl
+
+      assert :ko == :ok
+    end
+
+    test "spawns persist_patient_invitation_notification process" do
+      # TODO: Impl
+
       assert :ko == :ok
     end
   end
 
-  describie "persist_patient_invitation_notification/1" do
-    descrbie "when invitation SMS was sent" do
-      test "sends SMS to patient" do
-        assert :ko == :ok
-      end
+  describe "persist_patient_invitation_notification/1" do
+    test "when invitation SMS was sent it does not insert new record" do
+      # We deliberately don't insert patient with associated patient_invitation_notification
+      # Because we want to test, how system behave when patient data was destroyed
+      patient = insert(:patient)
+      patient_invitation_notification = insert(:patient_invitation_notification, %{phone: patient.phone})
+
+      result = ColomboCodModule.Services.NotifyPatientService.call(patient)
+
+      # TODO: Impl testing if nothing was inserted to db
+      
+      assert {:ko, :unprocessable} == result
     end
 
-    descrbie "when invitation SMS was not sent" do
-      test "creates new patient_invitation_notification association for patient" do
-        assert :ko == :ok
-      end
+    test "when invitation SMS was not sent it insert new record" do
+      patient = insert(:patient)
+
+      result = ColomboCodModule.Services.NotifyPatientService.call(patient)
+
+      # TODO: Impl testing if record was inserted to db
+
+      assert {:ok, :processed} == result
     end
   end
 end
